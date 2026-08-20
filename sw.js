1c74d34ac3dd39c7781f3dd676e9487fcd04e8c8
rahmenihamdi2026 <rahmenihamdi2026@gmail.com>
2026-08-21 00:11:20 +0100

    Update Service Worker version to v17

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Service Worker — Hamdi & Yossr 💍 — v16
+// Service Worker — Hamdi & Yossr 💍 — v17
 
 // ===== FCM Push (notif w l app msakra) =====
 importScripts("https://www.gstatic.com/firebasejs/11.6.0/firebase-app-compat.js");
@@ -28,7 +28,7 @@ self.addEventListener("notificationclick", event => {
   event.waitUntil(clients.openWindow("./index.html"));
 });
 
-const CACHE_NAME = "hamdi-yossr-v16";
+const CACHE_NAME = "hamdi-yossr-v17";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
